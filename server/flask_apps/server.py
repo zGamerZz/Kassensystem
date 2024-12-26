0005d3f4d4150797531adca75a6b1044c429c600
zGamerZz <h.elghachtouli@gmail.com>
2024-12-26 14:47:41 +0100

    modified:   requirements.txt 	modified:   server/flask_apps/server.py 	modified:   server/frontend/base.html 	modified:   server/frontend/index.html

--- server/flask_apps/server.py
+++ server/flask_apps/server.py
@@ -124,4 +124,4 @@ if __name__ == "__main__":
     threading.Thread(target=start_api, daemon=True).start()
 
     # Starte die Hauptanwendung
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
